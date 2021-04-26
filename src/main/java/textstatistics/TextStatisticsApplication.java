007153a7457034b26f8d9f66e4542e52fb927849
dtiern5 <dtierney@fastmail.com>
2021-04-26 16:24:23 -0500

    added listener to track words, characters, and longest word

--- src/main/java/textstatistics/TextStatisticsApplication.java
+++ src/main/java/textstatistics/TextStatisticsApplication.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class TextStatisticsApplication extends Application {
 
@@ -17,13 +18,32 @@ public class TextStatisticsApplication extends Application {
     public void start(Stage stage) throws Exception {
         BorderPane layout = new BorderPane();
 
+        TextArea textArea = new TextArea();
+
+        Label letters = new Label("Letters: 0");
+        Label words = new Label("Words: 0");
+        Label longest = new Label("The longest word is:");
+
+
+        textArea.textProperty().addListener((change, oldValue, newValue) -> {
+            int characters = newValue.length();
+            String[] parts = newValue.split(" ");
+            String longestWord = Arrays.stream(parts)
+                    .sorted((s1, s2) -> s2.length() - s1.length())
+                    .findFirst()
+                    .get();
+            letters.setText("Letters: " + String.valueOf(characters));
+            words.setText("Words: " + String.valueOf(parts.length));
+            longest.setText("The longest word is: " + longestWord);
+        });
+
         HBox bottomHBox = new HBox();
         bottomHBox.setSpacing(10);
-        bottomHBox.getChildren().add(new Label("Letters: 0"));
-        bottomHBox.getChildren().add(new Label("Words: 0"));
-        bottomHBox.getChildren().add(new Label("The longest word is:"));
+        bottomHBox.getChildren().add(letters);
+        bottomHBox.getChildren().add(words);
+        bottomHBox.getChildren().add(longest);
 
-        layout.setCenter(new TextArea(""));
+        layout.setCenter(textArea);
         layout.setBottom(bottomHBox);
 
         Scene scene = new Scene(layout);
