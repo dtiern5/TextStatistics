b06eb467d6552946a39a3da29a9335258c748089
dtiern5 <dtierney@fastmail.com>
2021-04-26 16:12:09 -0500

    set up layout

--- src/main/java/textstatistics/TextStatisticsApplication.java
+++ src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,10 +1,39 @@
 package textstatistics;
 
 
-public class TextStatisticsApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
+import java.awt.*;
+
+public class TextStatisticsApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane layout = new BorderPane();
+
+        HBox bottomHBox = new HBox();
+        bottomHBox.setSpacing(10);
+        bottomHBox.getChildren().add(new Label("Letters: 0"));
+        bottomHBox.getChildren().add(new Label("Words: 0"));
+        bottomHBox.getChildren().add(new Label("The longest word is:"));
+
+        layout.setCenter(new TextArea(""));
+        layout.setBottom(bottomHBox);
+
+        Scene scene = new Scene(layout);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
+        launch(TextStatisticsApplication.class);
         System.out.println("Hello world!");
     }
 
